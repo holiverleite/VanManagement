0f9e8164ffffb94f8d17f06410beb4cf557a0167
Haroldo Leite <holiverleite@gmail.com>
2019-06-28 11:23:47 -0300

    Initial commit - Implementing the inital navigation between some screens.

---
+++ app/src/main/java/br/edu/ifsp/scl/vanmanagement/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.edu.ifsp.scl.vanmanagement
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun userProfile(view: View) {
+        val userIntent = Intent(this, UserProfileActivity::class.java)
+        startActivity(userIntent)
+    }
+}
